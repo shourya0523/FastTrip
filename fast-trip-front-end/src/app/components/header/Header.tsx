c838f7d57c36c37f927b5b4a77e9a78c57c77167
robshow <robsonaraujocarmo@hotmail.com>
2025-06-22 01:49:50 -0400

    feat: header

--- fast-trip-front-end/src/app/components/header/Header.tsx
+++ fast-trip-front-end/src/app/components/header/Header.tsx
@@ -287,8 +287,11 @@ export default function Header() {
             <ComingSoon style={{ marginTop: "15px" }} width="60" />
           </div>
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+        <div className="hidden flex  items-center  lg:flex lg:flex-1 lg:justify-end">
+          <a
+            href="#"
+            className="cursor-not-allowed  text-sm/6 font-semibold text-gray-900"
+          >
             Log in <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
